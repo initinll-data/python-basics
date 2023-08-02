221f81ab5d4479115e9e328dd9e520df0065b758
initinll <initinli@yahoo.com>
2023-08-03 01:27:33 +0530

    added 10_properties.py

---
+++ 10_properties.py
@@ -0,0 +1,66 @@
+# Using Getters and Setters
+# Making Getters and Setter methods
+class Celsius1:
+    def __init__(self, temperature=0):
+        self.set_temperature(temperature)
+
+    def to_fahrenheit(self):
+        return (self.get_temperature() * 1.8) + 32
+
+    # getter method
+    def get_temperature(self):
+        return self._temperature
+
+    # setter method
+    def set_temperature(self, value):
+        if value < -273.15:
+            raise ValueError("Temperature below -273.15 is not possible.")
+        self._temperature = value
+
+
+# Create a new object, set_temperature() internally called by __init__
+human = Celsius1(37)
+
+# Get the temperature attribute via a getter
+print(human.get_temperature())
+
+# Get the to_fahrenheit method, get_temperature() called by the method itself
+print(human.to_fahrenheit())
+
+# new constraint implementation
+human.set_temperature(-300)
+
+# Get the to_fahreheit method
+print(human.to_fahrenheit())
+
+
+# The @property Decorator
+# Using @property decorator
+class Celsius2:
+    def __init__(self, temperature=0):
+        self.temperature = temperature
+
+    def to_fahrenheit(self):
+        return (self.temperature * 1.8) + 32
+
+    @property
+    def temperature(self):
+        print("Getting value...")
+        return self._temperature
+
+    @temperature.setter
+    def temperature(self, value):
+        print("Setting value...")
+        if value < -273.15:
+            raise ValueError("Temperature below -273 is not possible")
+        self._temperature = value
+
+
+# create an object
+human = Celsius2(37)
+
+print(human.temperature)
+
+print(human.to_fahrenheit())
+
+coldest_thing = Celsius2(-300)
